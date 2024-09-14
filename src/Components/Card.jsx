80228a1bd73dba3f2758ef5501d820fa7597a537
Girish-Gaurav-Sharma <135326401+Girish-Gaurav-Sharma@users.noreply.github.com>
2024-09-14 09:44:31 +0530

    Docs.

---
+++ src/Components/Card.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import { FaRegFileAlt } from "react-icons/fa";
+import { LuDownload } from "react-icons/lu";
+import { IoIosClose } from "react-icons/io";
+import { motion } from "framer-motion"
+
+function Card({dataForCard , refrence}) {
+  return (
+    <motion.div drag dragConstraints={refrence} whileDrag={{scale: 1.2}} dragElastic={0.2} 
+    dragTransition={{ bounceStiffness: 100, bounceDamping: 30 }} className='relative shrink-0 px-8 py-10 leading-tight w-52 h-64 rounded-[45px] bg-zinc-900/90 text-white overflow-hidden'>
+        <FaRegFileAlt />
+        <p className='mt-3 text-sm font-semibold'>{dataForCard.desc} </p>
+        <div className='footer absolute w-full bottom-0 left-0 '>
+            <div className='flex items-center justify-between px-8 mb-2'>
+                <h5>{dataForCard.filesize}</h5>
+                <span className='w-7 h-7 bg-zinc-600 rounded-full flex items-center justify-center'>
+                    {dataForCard.close ? <IoIosClose />: <LuDownload size='.7em' color='#fff'/>}
+                </span>
+            </div>
+            {
+                dataForCard.tag.isOpen &&(
+            <div className={`tag w-full py-4 bg-${dataForCard.tag.tagColor}-600 flex items-center justify-center`}>
+                <h3 className='text-sm font-semibold'>{dataForCard.tag.tagtitle}</h3>
+            </div>
+
+                )
+            }
+
+        </div>    
+            
+    </motion.div>
+  )
+}
+
+export default Card
---
+++ src/Components/Foreground.jsx
@@ -0,0 +1,51 @@
+import React, { useRef } from 'react'
+import Card from './Card'
+
+function Foreground() {
+    const ref = useRef(null)
+const data = [
+    {
+        desc: 'This is the content of the card to be displayed inside the card',
+        filesize: '.9mb', 
+        close: true,
+        tag:{
+            isOpen: true,
+            tagtitle: 'Download Now', 
+            tagColor: 'green'
+        }
+
+    },
+    {
+        desc: 'This is the content of the card to be displayed inside the card',
+        filesize: '.9mb', 
+        close: true,
+        tag:{
+            isOpen: true,
+            tagtitle: 'Download Now', 
+            tagColor: 'zinc'
+        }
+
+    },
+    {
+        desc: 'This is the content of the card to be displayed inside the card',
+        filesize: '.9mb', 
+        close: true,
+        tag:{
+            isOpen: true,
+            tagtitle: 'Download Now', 
+            tagColor: 'blue'
+        }
+
+    }
+    
+]
+    return (
+    <div ref={ref} className='fixed top-0 left-0 h-full w-full z-[3] flex gap-10 flex-wrap p-5'>
+        {data.map((item, index)=>(
+            <Card dataForCard={item} refrence={ref} />
+        ))}
+    </div>
+  )
+}
+
+export default Foreground
